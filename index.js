bc141661bc3bee8412aa161c11970eb7941b1da6
Игорь Бабушкин <ibabushkin@elma-bpm.com>
2026-01-23 18:21:30 +0400

    fix promise

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 import { pageLoaderAction } from './src/actions/index.js'
 
 export default pageLoaderAction
-
-pageLoaderAction('https://elma365')
-  .then(msg => console.log(msg))
-  .catch(err => console.log(err))
