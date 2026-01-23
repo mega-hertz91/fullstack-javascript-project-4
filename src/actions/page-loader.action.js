ff54516b8786d52e57f569b248901fecc9482e16
Игорь Бабушкин <ibabushkin@elma-bpm.com>
2026-01-23 11:40:56 +0400

    add loader

--- src/actions/page-loader.action.js
+++ src/actions/page-loader.action.js
@@ -1,8 +1,9 @@
 import { dirname, join } from 'node:path'
 import { fileURLToPath } from 'node:url'
+import { AxiosError } from 'axios'
+import Listr from 'listr'
 import { AxiosService, FSService, DomService } from '../services/index.js'
 import { createNameFromUrl } from '../utils/index.js'
-import { AxiosError } from 'axios'
 
 export default (url, { output = process.cwd() }) => {
   const __dirname = dirname(fileURLToPath(import.meta.url))
@@ -30,8 +31,11 @@ export default (url, { output = process.cwd() }) => {
     .then(() => Dom.extractResources())
     .then(({ images, scripts, links }) => [...images, ...scripts, ...links].map(item => URL.parse(item) ? null : item))
     .then(src => src.filter(item => item !== null))
-    .then(src => src.map(item => AxiosService.downloadFile(url + item, workDir + '/' + item)))
-    .then(promises => Promise.all(promises))
+    .then(src => new Listr(src.map(item => ({
+      title: 'Download source: ' + item,
+      task: () => AxiosService.downloadFile(url + item, workDir + item),
+    }))))
+    .then(tasks => tasks.run())
     .catch((err) => {
       if (err instanceof AxiosError) {
         const { config, message } = err
